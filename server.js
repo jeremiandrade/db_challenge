34ae877ceb0ec5b2ba1cecca8462f6ee443ba89e
jeremiandrade <jeremiandrade04@gmail.com>
2026-07-01 22:49:34 +0200

    feat: implement user routes and SQL queries for user data retrieval

--- server.js
+++ server.js
@@ -12,3 +12,34 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the API' })
 })
+
+
+// Sviluppa un semplice server REST API con Express, connettilo al database mysql ed implementa le seguenti rotte:
+
+// - `GET /users` - restituisce tutti gli utenti
+//INDEX
+app.get('/users', (req, res) => {
+    // Implement logic to fetch all users from the database
+    res.json({ message: 'Returning all users' });
+});
+
+// - `GET /users/:id` - restituisce un utente specifico
+//SHOW
+app.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    console.log(userId)
+    //cerco users con id corrispondente 
+    const user = users.findd(user => users.id === userId)
+    if (user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
+
+})
+
+
+
+
+// - `GET /posts` - restituisce tutti i post
+// - `GET /posts/:id` - restituisce un post specifico
+
+
